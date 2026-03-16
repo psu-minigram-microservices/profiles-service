dbece8b3c29c3a98df5b7d94135ee0dc66fcfb2a
Wahidsze <Wahidsze@gmail.com>
2026-03-16 22:05:54 +0500

    Removed RequestLogginMiddleware and excessive logging

--- backend/Minigram/Minigram.Core/Middleware/MiddlewareExtensions.cs
+++ backend/Minigram/Minigram.Core/Middleware/MiddlewareExtensions.cs
@@ -4,11 +4,6 @@
 
     public static class MiddlewareExtensions
     {
-        public static IApplicationBuilder UseRequestLogging(this IApplicationBuilder app)
-        {
-            return app.UseMiddleware<RequestLoggingMiddleware>();
-        }
-
         public static IApplicationBuilder UseExceptionHandling(this IApplicationBuilder app)
         {
             return app.UseMiddleware<ExceptionHandlingMiddleware>();
